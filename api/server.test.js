f3951c88451f447599d565f01daa1972dd0ed223
Will B <wberman27@gmail.com>
2021-04-23 11:00:02 -0400

    added testing for GET jokes endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,3 @@
-// Write your tests here
 const request = require('supertest')
 const server = require('./server')
 const db = require('../data/dbConfig')
@@ -6,12 +5,29 @@ const db = require('../data/dbConfig')
 const userOne = {username:"Will", password:"1234"}
 const userTwo = {username:"Max", password:"1234"}
 
-test('sanity', () => {
-  expect(true).toBe(false)
+beforeAll(async ()=>{
+  await db.migrate.rollback() //rollback and rebuild table
+  await db.migrate.latest()
 })
+
+beforeEach(async ()=>{
+  await db("users").truncate() //gets rid of data in users table (table remains intact)
+
+})
+afterAll(async ()=>{
+  await db.destroy() //cuts off connection to db
+})
+
+//tests
+
+test('sanity', () => {
+  expect(true).toBe(true)
+})
+
+//API endpoints
 describe("[POST] /auth/register testing", ()=>{
   it("tests the response", ()=>{
-
+    
   })
   it("testing more", ()=>{
     
@@ -26,10 +42,24 @@ describe("[POST] /auth/login testing", ()=>{
   })
 })
 describe("[GET] /jokes testing", ()=>{
-  it("tests the response", ()=>{
-
+  it("tests the response to be 200 ok with auth", async ()=>{
+    await request(server).post('/api/auth/register').send(userOne)
+    const login = await request(server).post('/api/auth/login').send(userOne)
+    const token = login.body.token
+    const res = await request(server).get('/api/jokes').set('Authorization', token)
+    expect(res.status).toBe(200)
   })
-  it("testing more", ()=>{
-
+  it("tests the response error to be 401 with no token", async ()=>{
+    const res = await request(server).get('/api/jokes')
+    expect(res.status).toBe(401)
+  })
+  it("tests the response message to be token required with no token", async ()=>{
+    const res = await request(server).get('/api/jokes')
+    expect(res.body).toBe("token required")
+  })
+  it("tests the response message to be token invalid with wrong token", async ()=>{
+    const token = "1234567890WDAWDWADWADWADWADWASDASDASDASD"
+    const res = await request(server).get('/api/jokes').set('Authorization', token)
+    expect(res.body).toBe("token invalid")
   })
 })
