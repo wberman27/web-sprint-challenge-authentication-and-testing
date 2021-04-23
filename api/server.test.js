07a71afcfd30614a2e9c10dbbc486906df35f475
Will B <wberman27@gmail.com>
2021-04-23 11:12:37 -0400

    added testing for register user endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest')
 const server = require('./server')
 const db = require('../data/dbConfig')
+const { checkUserPass } = require('./middleware/checkUserPass')
+const Users = require('./users/users-model')
 
 const userOne = {username:"Will", password:"1234"}
 const userTwo = {username:"Max", password:"1234"}
@@ -26,11 +28,24 @@ test('sanity', () => {
 
 //API endpoints
 describe("[POST] /auth/register testing", ()=>{
-  it("tests the response", ()=>{
-    
+  it("tests the response of register to be correct object format", async ()=>{
+    const res = await request(server).post('/api/auth/register').send(userOne)
+    const hashedPass = res.body.password;
+    expect(res.body).toMatchObject({id:1, username:"Will", password:hashedPass})
   })
-  it("testing more", ()=>{
-    
+  it("tests that the username must be unique", async ()=>{
+    await request(server).post('/api/auth/register').send(userOne)
+    const res = await request(server).post('/api/auth/register').send(userOne)
+    expect(res.body).toBe("username taken")
+  })
+  it("tests that user was added to the db", async ()=>{
+    await request(server).post('/api/auth/register').send(userOne)
+    const user = await Users.find();
+    expect(user).toHaveLength(1);
+
+    await request(server).post('/api/auth/register').send(userTwo)
+    const users = await Users.find();
+    expect(users).toHaveLength(2);
   })
 })
 describe("[POST] /auth/login testing", ()=>{
